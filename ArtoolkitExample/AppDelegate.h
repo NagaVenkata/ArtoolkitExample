4a9fb39b337b6607f9aa444f95a28dddb3659807
Umapathi <Umapathi@c-77-133.eduroam.liu.se>
2013-01-31 20:57:32 +0100

    Initial Commit

---
+++ ArtoolkitExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ArtoolkitExample
+//
+//  Created by Umapathi on 1/31/13.
+//  Copyright (c) 2013 Umapathi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ArtoolkitExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ArtoolkitExample
+//
+//  Created by Umapathi on 1/31/13.
+//  Copyright (c) 2013 Umapathi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
